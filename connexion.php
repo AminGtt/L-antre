51d1d21c4a6aae79033415f6d45936dce847b1fb
AminGtt <gatta.amin38@gmail.com>
2020-10-27 23:58:18 +0100

    inscription et connexion OK ---- Bug requete INSERT INTO

--- connexion.php
+++ connexion.php
@@ -1,24 +1,69 @@
-<?php require __DIR__ . "/templates/header.tpl.php" ?>
+<?php require __DIR__ . "/templates/header.tpl.php";
 
-<!-- code here! -->
-<form action="">
-  <div class="container text-center mt-5 mb-5">
-    <p class="font-weight-bolder">Alors comme ça il parait que vous faites partis des nôtres? Prouvez-le!</p>
+$bdd = new PDO('mysql:host=localhost;dbname=u_shall_not_pass', 'root', 'toor');
 
-    <div class="form-group">
-      <label for="mail">Email : </label>
-      <input type="mail">
-    </div>
+if(isset($_POST['sub']))
+{
+  $mail = htmlspecialchars($_POST['mail']);
+  $passH = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
-    <div class="form-group">
-      <label for="pass">Mot de passe : </label>
-      <input type="password">
-    </div>
+  if(filter_var($mail, FILTER_VALIDATE_EMAIL))
+  {
+    $mailToValidate = $bdd->prepare('SELECT * FROM users WHERE mail=?');
+    $mailToValidate->execute([$mail]);
+    $mailExist = $mailToValidate->rowCount();
+    
+    if($mailExist === 1)
+    {
+      $passToValidate = $bdd->prepare('SELECT * FROM users WHERE passH=?');
+      $passToValidate->execute([$passH]);
+      $passExist = $passToValidate->rowCount();
+      if(password_verify($passExist, $passH))
+      {
+        echo 'Le mot de passe est valide!';
+      }
+      else
+      {
+        $error = 'Mot de passe incorect!';
+      }
+    }
+    else
+    {
+      $error = 'Mail incorrect!';
+    }
+  }
+}
 
-    <input type="submit" value="Connexion" class='btn btn-lg btn-success'>
+?>
+
+<form action="" method="POST">
+  <div class="container text-center">
+    <h3 class="text-center mt-5 mb-4 font-weight-bolder">Alors comme ça il parait que vous faites partis des nôtres? Prouvez-le!</h3>
+
+    <table class="table table-striped table-bordered table-hover col-4 ml-auto mr-auto">
+      <tr>
+        <td align="right"><label for="mail">Email : </label></td>
+        <td align="left"><input type="mail" name="mail" id="mail" autocomplete="off"></td>
+      </tr>
+      <tr>
+        <td align="right"><label for="pass">Mot de passe : </label></td>
+        <td align="left"><input type="password" name="pass" id="pass" autocomplete="off"></td>
+      </tr>
+    </table>
+
+    <input type="submit" value="Connexion" class='btn btn-lg btn-success mt-3' name="sub">
 
   </div>
 </form>
+
+<div align='center' style="color:red;" class="font-weight-bolder mt-3">
+  <?php
+    if(isset($error))
+    {
+      echo $error;
+    }
+  ?>
+</div>
 
 
 <?php require __DIR__ . "/templates/footer.tpl.php" ?>
--- enregistrement.php
+++ enregistrement.php
@@ -4,16 +4,18 @@ $bdd = new PDO('mysql:host=localhost;dbname=u_shall_not_pass', 'root', 'toor');
 
 if(isset($_POST['sub']))
 {
+  echo 'ok';
+  
   $pseudo = htmlspecialchars($_POST['pseudo']);
   $bDay = htmlspecialchars($_POST['bDay']);
   $mail = htmlspecialchars($_POST['mail']);
   $mailBis = htmlspecialchars($_POST['mailBis']);
   $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
   $passBis = password_hash($_POST['passBis'], PASSWORD_DEFAULT);
-  $major = htmlspecialchars($_POST['major']);
-
+  
   if(!empty($_POST['pseudo']) AND !empty($_POST['bDay']) AND !empty($_POST['mail']) AND !empty($_POST['mailBis']) AND !empty($_POST['pass']) AND !empty($_POST['passBis']) AND !empty($_POST['major']))
   {
+    $major = htmlspecialchars($_POST['major']);
     $pseudoLenght = strlen($pseudo);
     if($pseudoLenght <= 50)
     {
@@ -21,15 +23,25 @@ if(isset($_POST['sub']))
       {
         if(filter_var($mail, FILTER_VALIDATE_EMAIL))
         {
-          if($_POST['pass'] === $_POST['passBis'])
+          $reqMail = $bdd->prepare('SELECT * FROM users WHERE mail=?');
+          $reqMail->execute(array($mail));
+          $mailExist = $reqMail->rowCount();
+          if($mailExist == 0)
           {
-            $insertMember = $bdd->prepare('INSERT INTO users(pseudo, mail, passwordH, bDay) VALUES(?, ?, ?, ?)');
-            $insertMember->execute(array($pseudo, $mail, $pass, $bDay));
-            $success = "Votre compte à bien été créé.";
+            if($_POST['pass'] === $_POST['passBis'])
+            {
+              $insertMember = $bdd->prepare('INSERT INTO users(pseudo, mail, passwordH, bDay) VALUES(?, ?, ?, ?)');
+              $insertMember->execute([$_POST['pseudo'], $_POST['mail'], $pass, $_POST['bDay']]);
+              $success = "Votre compte à bien été créé.";
+            }
+            else
+            {
+              $error = "Vos mots de passe ne correspondent pas.";
+            }
           }
           else
           {
-            $error = "Vos mots de passe ne correspondent pas.";
+            $error = 'Votre mail est deja utilisé.';
           }
         } 
         else
@@ -56,48 +68,55 @@ if(isset($_POST['sub']))
 ?>
 
 <form method="POST" action="">
+
   <div class="container text-center">
+
     <h3 class="text-center mt-5 mb-4 font-weight-bolder">Hmm, vous voulez faire parti de l'équipe? Renseignez les informations ci-dessous et vous en êtes. Du moins c'est ce qu'on vera x)</h3>
 
-    <div class='form-group'>
-      <label for="pseudo">Pseudo : </label>
-      <input type="text" name="pseudo" id="pseudo" value="<?php if(isset($pseudo)){echo "$pseudo";} ?>">
-    </div>
+    <table class="table table-striped table-bordered table-hover col-4 ml-auto mr-auto">
 
-    <div class='form-group'>
-      <label for="bDay">Date de naissance (jj/mm/aaaa) : </label>
-      <input type="text" name="bDay" id="bDay" value="<?php if(isset($bDay)){echo "$bDay";} ?>">
-    </div>
+      <tr>
+        <td align="right"><label for="pseudo">Pseudo : </label></td>
+        <td align="left"><input type="text" name="pseudo" id="pseudo" value="<?php if(isset($pseudo)){echo "$pseudo";} ?>" autocomplete="off"></td>
+      </tr>
 
-    <div class='form-group'>
-      <label for="mail">Adresse e-mail : </label>
-      <input type="mail" name="mail" id="mail" value="<?php if(isset($mail)){echo "$mail";} ?>">
-    </div>
+      <tr>
+        <td align="right"><label for="bDay">Date de naissance (jj/mm/aaaa) : </label></td>
+        <td align="left"><input type="text" name="bDay" id="bDay" value="<?php if(isset($bDay)){echo "$bDay";} ?>" autocomplete="off"></td>
+      </tr>
 
-    <div class='form-group'>
-      <label for="mailBis">Repeter l'adresse e-mail : </label>
-      <input type="mail" name="mailBis" id="mailBis" value="<?php if(isset($mailBis)){echo "$mailBis";} ?>">
-    </div>
+      <tr>
+        <td align="right"><label for="mail">Adresse e-mail : </label></td>
+        <td align="left"><input type="mail" name="mail" id="mail" value="<?php if(isset($mail)){echo "$mail";} ?>" autocomplete="off"></td>
+      </tr>
 
-    <div class='form-group'>
-      <label for="pass">Mot de passe : </label>
-      <input type="password" name="pass" id="pass">
-    </div>
+      <tr>
+        <td align="right"><label for="mailBis">Repeter l'adresse e-mail : </label></td>
+        <td align="left"><input type="mail" name="mailBis" id="mailBis" value="<?php if(isset($mailBis)){echo "$mailBis";} ?>" autocomplete="off"></td>
+      </tr>
 
-    <div class='form-group'>
-      <label for="passBis">Repeter le mot de passe : </label>
-      <input type="password" name="passBis" id="passBis">
-    </div>
+      <tr>
+        <td align="right"><label for="pass">Mot de passe : </label></td>
+        <td align="left"><input type="password" name="pass" id="pass" autocomplete="new-password"></td>
+      </tr>
 
+      <tr>
+        <td align="right"><label for="passBis">Repeter le mot de passe : </label></td>
+        <td align="left"><input type="password" name="passBis" id="passBis" autocomplete="new-password"></td>
+      </tr>
+
+    </table>
+    
     <div class='form-check'>
       <input class='form-check-input' type="checkbox" name="major" id="major">
       <label class='form-check-label ml-3' for="major" id="major">En validant ce formulaire vous assurez avoir plus de 18ans.</label>
     </div>
 
-    <input class='btn btn-lg btn-primary mt-3' type="submit" value="Inscription" name="sub">
+    <input class='btn btn-lg btn-outline-dark mt-3' type="submit" value="Inscription" name="sub">
 
   </div>
 </form>
+
 <div align='center' style="color:red;" class="font-weight-bolder mt-3">
   <?php
     if(isset($error))
