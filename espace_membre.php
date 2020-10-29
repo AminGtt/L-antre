a637e2bef7d017007992e21e1e356e3f4df282f8
AminGtt <gatta.amin38@gmail.com>
2020-10-29 23:03:14 +0100

    Inscription et connexion OK manyy bugs incoming

--- espace_membre.php
+++ espace_membre.php
@@ -1,6 +1,11 @@
 <?php require __DIR__ . "/templates/header.tpl.php" ?>
 
-<h1>Welcome Home :)</h1>
+<h1 align='center'>Welcome Home :)</h1>
+
+<main>
+  <p>Bienvenue sur ta super page d'acceuil qui sera bientot personaliser! :)</p>
+  <p>Tu auras accées a tout un tas de contenu spécialement preparé pour toi par le grand Chat Fou en personne !</p>
+</main>
 
 <div align='center'>
   <a class='btn btn-lg btn-outline-dark mt-3' href="index.php">Retour à l'acceuil</a>
